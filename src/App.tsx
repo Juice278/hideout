b5275ca635a463774d3f30f8b315ee25e6bc21a7
Juice278 <102438552+Juice278@users.noreply.github.com>
2022-04-03 21:57:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  Flex,
   theme,
+  Input,
+  Heading,
+  useColorModeValue,
+  useColorMode,
+  Button,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
-export const App = () => (
+export const App = () => {
+const  { toggleColorMode } = useColorMode()
+const formBackground = useColorModeValue("gray.200", "gray.700")
+return (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+
+  <Flex height="100vh" alignItems="center" justifyContent="center">
+   <Flex direction="column" background={formBackground} p={12} rounded={6}>
+     <Heading mb={6}>log in</Heading>
+     <Input placeholder="Enter your email address" variant="filled" mb={3} type="email" />
+     <Input placeholder="Enter your password" variant="filled" mb={6} type="password" />
+     <Button mb={6} colorScheme="teal">Log in</Button>
+     <Button onClick={toggleColorMode}> Toggle Color Mode </Button>
+   </Flex>
+  </Flex>
   </ChakraProvider>
 )
+}
