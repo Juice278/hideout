ab2b9f2fdef00bb82b6821b58466a753a40f6b2d
Juice278 <102438552+Juice278@users.noreply.github.com>
2022-04-03 22:30:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,20 @@ import {
   theme,
   Input,
   Heading,
-  useColorModeValue,
-  useColorMode,
   Button,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 
 export const App = () => {
-const  { toggleColorMode } = useColorMode()
-const formBackground = useColorModeValue("gray.200", "gray.700")
 return (
   <ChakraProvider theme={theme}>
     <ColorModeSwitcher justifySelf="flex-end" />
   <Flex height="100vh" alignItems="center" justifyContent="center">
-   <Flex direction="column" background={formBackground} p={12} rounded={6}>
+   <Flex direction="column" background={gray.200} p={12} rounded={6}>
      <Heading mb={6}>log in</Heading>
      <Input placeholder="Enter your email address" variant="filled" mb={3} type="email" />
      <Input placeholder="Enter your password" variant="filled" mb={6} type="password" />
      <Button mb={6} colorScheme="teal">Log in</Button>
-     <Button onClick={toggleColorMode}> Toggle Color Mode </Button>
    </Flex>
   </Flex>
   </ChakraProvider>
